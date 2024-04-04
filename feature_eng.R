d673b9f76552fbc6d777b7f3b9a0c3982b71c378
JD1608 <frigo.jeandavid@gmail.com>
2024-04-04 10:30:16 +0200

    Update feature_eng.R

--- feature_eng.R
+++ feature_eng.R
@@ -30,68 +30,53 @@ na_count <-sapply(df4, function(y) sum(length(which(is.na(y)))))
 na_count <- data.frame(na_count)
 
 
-#analyse descriptive de notre portefeuille 
+#impute missing Value
+library(mice)
+df4$Type_fuel <- as.factor(df4$Type_fuel)
+md.pattern(df4)
+imputedData <- mice(df4, method = "rf", m = 1, maxit = 5)
+complet<-complete(imputedData)
+md.pattern(complet)
 
-#frequence de sinistre
-ggplot(dataset, aes(x=N_claims_year))+
-  geom_bar()+
-  geom_label(stat='count', 
-             aes(label =  percent(prop.table(after_stat(count)), 
-                                  accuracy = 0.01)),
-             vjust = 0.5)+
-  scale_x_continuous(name = "Number of Claims")+
-  scale_y_continuous(name = "Number of Polices", 
-                     labels = label_number())+
-  ggtitle("Proportion of policies by number of claims")
+stripplot(complet, pch = 1, cex = 1.2)
+densityplot(imputedData)
 
 
-
-#premium
-ggplot(db_final, aes(x = Premium)) + 
-  geom_histogram(binwidth = 10, fill = "blue", color = "black") +
-  labs(x = "Nom de Variable", y = "Fréquence") +
-  ggtitle("Titre du Graphique")
-#premium with h= 10 we can see a concentration of obervations around 250 euros 
-
-#type de fuel
-
-ggplot(db_final, aes(x = Type_fuel)) + 
-  geom_bar(fill = "blue") +
-  labs(x = "Nom de Variable", y = "Compte") +
-  ggtitle("Nom de Variable")
+stripplot(imputedData, "Type_fuel", pch = 20, cex = 1.2)
+stripplot(imputedData, "Length", pch = 20, cex = 1.2)
 
 
-ggplot(db_final, aes(x = Type_fuel, y = Premium)) + 
-  geom_boxplot() +
-  labs(x = "Nom de Variable Catégorielle", y = "Nom de Variable Numérique") +
-  ggtitle("Relation entre  et ")
+# Utilisation de xyplot pour visualiser les imputations
 
-
-ggplot(db_final, aes(x = N_doors, y = N_claims_year)) + 
-  geom_boxplot() +
-  labs(x = "Nom de Variable Catégorielle", y = "Nom de Variable Numérique") +
-  ggtitle("Relation entre  et ")
-
-ggplot(db_final, aes(x = N_doors)) + 
-  geom_histogram(binwidth = 1, fill = "blue", color = "black") +
-  labs(x = "Nom de Variable", y = "Fréquence") +
-  ggtitle("claims by n_doors")
+write.csv(complet, "C:/Users/jeand/Desktop/travail perso unif/cours maths et eco/db_final.csv", row.names = FALSE)
+library(readr)
+db_final <- read_csv("C:/Users/jeand/Desktop/dataset multivarie/db_final.csv")
+View(db_final)
 
 
 
-
-# Calculer le tableau de contingence
-tableau <- table(db_final$N_doors, db_final$N_claims_year)
-ggplot(data = db_final, aes(x = factor(N_doors), fill = factor(N_claims_year))) +
-  geom_bar(position = "fill") +
-  labs(x = "nb_doors", y = "Proportion", fill = "nb claims") +
-  theme_minimal() +
-  coord_flip()
-# Afficher le tableau
-print(tableau)
+db_final <- db_final %>%
+  mutate(Type_risk = case_when(
+    Type_risk == 1 ~ "motorbikes",
+    Type_risk == 2 ~ "vans",
+    Type_risk == 3 ~ "passenger cars",
+    Type_risk == 4 ~ "agricultural vehicles",
+    TRUE ~ as.character(Type_risk)  # Pour gérer les éventuels cas non couverts
+  ))
 
 
-ggplot(db_final, aes(x = Type_fuel, y = Premium)) + 
-  geom_boxplot() +
-  labs(x = "Nom de Variable Catégorielle", y = "Nom de Variable Numérique") +
-  ggtitle("Relation entre  et ")
+db_final <- db_final %>%
+  mutate(Area = case_when(
+    Area == 0 ~ "rural",
+    Area == 1 ~ "urban",
+    TRUE ~ as.character(Area)  # Pour gérer les éventuels cas non couverts
+  ))
+
+
+
+db_final <- db_final %>%
+  mutate(Second_driver= case_when(
+    Second_driver == 0 ~ "no",
+    Second_driver == 1 ~ "yes",
+    TRUE ~ as.character(Area)  # Pour gérer les éventuels cas non couverts
+  ))
