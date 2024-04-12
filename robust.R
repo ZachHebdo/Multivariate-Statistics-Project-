99dcc772b90d4e8af5d169c11d392ba052684438
ZachHebdo <45517776+ZachHebdo@users.noreply.github.com>
2024-04-12 11:37:19 +0200

    Robust

--- robust.R
+++ robust.R
@@ -1,12 +1,18 @@
 ### Analyse robuste ----
+library(robuste)
+library(tidyverse)
+library(dplyr)
+
 data<-db_final
 dfquanti <- data %>% select(where(is.numeric))
 dfquali <- data %>% select(where(is.character))
+
 for(i in 1:ncol(dfquali)){
   tt <- table(dfquali[,i]) %>% as.data.frame() %>% arrange(desc(Freq))
   print(slice_head(tt, n = 8))
   print(nrow(tt))
 }
+
 for(i in 1:ncol(dfquanti)){
    DD <- as.data.frame(dfquanti[,i]) %>% filter(dfquanti[,i] > 0)
    med <- median(DD[,1],na.rm = TRUE)
