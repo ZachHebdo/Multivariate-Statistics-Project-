805331e6c5d1072726a7aa3fc00a0df00cc06f52
JD1608 <frigo.jeandavid@gmail.com>
2024-04-21 17:21:48 +0200

    Update insurance_data.R

--- insurance_data.R
+++ insurance_data.R
@@ -1,10 +1,18 @@
 #library(openxlsx)
-
+library(mice)
 library(tidyverse)
+library(dplyr)
 
-setwd("~/Studies/ULB - Masters/Multivariate Statistical Analysis/Project")
-#df <- read.xlsx('TF_VEHICLES_COLLISION_2022.xlsx')
+library(readr)
+db_final <- read_csv("db_final.csv")
+View(db_final)
 
+
+
+Motor_vehicle_insurance_data <- read_delim("Desktop/travail perso unif/cours maths et eco /multivariee/Motor vehicle insurance data.csv", 
+                                           delim = ";", escape_double = FALSE, trim_ws = TRUE)
+View(Motor_vehicle_insurance_data)
+df<-Motor_vehicle_insurance_data
 df <- read.csv('Motor vehicle insurance data.csv', sep = ";")
 df <- df %>% mutate(Year_Renewal = gsub("^.*/", "", Date_last_renewal)) %>% group_by(Year_Renewal) %>% group_nest() 
 df <- df %>% add_column(distinct_ID = c(n_distinct(df$data[[1]][1]), n_distinct(df$data[[2]][1]), n_distinct(df$data[[3]][1]), n_distinct(df$data[[4]][1])))
@@ -26,72 +34,130 @@ df4 <- df$data[[4]] %>% mutate(Start_year = as.integer(gsub("^.*/", "", Date_sta
 
 glimpse(df4)
 
+df4$Distribution_channel <- ifelse(df4$Distribution_channel == "00/01/1900", "0", df4$Distribution_channel) 
+
 na_count <-sapply(df4, function(y) sum(length(which(is.na(y)))))
 na_count <- data.frame(na_count)
 
+#impute missing Value getting rid of the NA values through random forest 
 
-#analyse descriptive de notre portefeuille 
+df4$Type_fuel <- as.factor(df4$Type_fuel)
+md.pattern(df4)
+imputedData <- mice(df4, method = "rf", m = 1, maxit = 5)
+complet<-complete(imputedData)
+md.pattern(complet)
 
-#frequence de sinistre
-ggplot(dataset, aes(x=N_claims_year))+
-  geom_bar()+
-  geom_label(stat='count', 
-             aes(label =  percent(prop.table(after_stat(count)), 
-                                  accuracy = 0.01)),
-             vjust = 0.5)+
-  scale_x_continuous(name = "Number of Claims")+
-  scale_y_continuous(name = "Number of Polices", 
-                     labels = label_number())+
-  ggtitle("Proportion of policies by number of claims")
+stripplot(complet, pch = 1, cex = 1.2)
+densityplot(imputedData)
 
 
+stripplot(imputedData, "Type_fuel", pch = 20, cex = 1.2)
+stripplot(imputedData, "Length", pch = 20, cex = 1.2)
+db_final<-complet
 
-#premium
-ggplot(db_final, aes(x = Premium)) + 
-  geom_histogram(binwidth = 10, fill = "blue", color = "black") +
-  labs(x = "Nom de Variable", y = "Fréquence") +
-  ggtitle("Titre du Graphique")
-#premium with h= 10 we can see a concentration of obervations around 250 euros 
+# Utilisation de xyplot pour visualiser les imputations
 
-#type de fuel
-
-ggplot(db_final, aes(x = Type_fuel)) + 
-  geom_bar(fill = "blue") +
-  labs(x = "Nom de Variable", y = "Compte") +
-  ggtitle("Nom de Variable")
+write.csv(complet, "C:/Users/jeand/Desktop/travail perso unif/cours maths et eco/db_final.csv", row.names = FALSE)
+library(readr)
+db_final <- read_csv("C:/Users/jeand/Desktop/dataset multivarie/db_final.csv")
+View(db_final)
+db_final = read_csv("db_final.csv")
 
 
-ggplot(db_final, aes(x = Type_fuel, y = Premium)) + 
-  geom_boxplot() +
-  labs(x = "Nom de Variable Catégorielle", y = "Nom de Variable Numérique") +
-  ggtitle("Relation entre  et ")
+# this part is to change the columns that have numbers while they are categorical
+# into purely categorical columns which will make it easier to run categorization
+
+db_final <- db_final %>%
+  mutate(Type_risk = case_when(
+    Type_risk == 1 ~ "motorbikes",
+    Type_risk == 2 ~ "vans",
+    Type_risk == 3 ~ "passenger cars",
+    Type_risk == 4 ~ "agricultural vehicles",
+    TRUE ~ as.character(Type_risk)  # Pour gérer les éventuels cas non couverts
+  ))
 
 
-ggplot(db_final, aes(x = N_doors, y = N_claims_year)) + 
-  geom_boxplot() +
-  labs(x = "Nom de Variable Catégorielle", y = "Nom de Variable Numérique") +
-  ggtitle("Relation entre  et ")
-
-ggplot(db_final, aes(x = N_doors)) + 
-  geom_histogram(binwidth = 1, fill = "blue", color = "black") +
-  labs(x = "Nom de Variable", y = "Fréquence") +
-  ggtitle("claims by n_doors")
+db_final <- db_final %>%
+  mutate(Area = case_when(
+    Area == 0 ~ "rural",
+    Area == 1 ~ "urban",
+    TRUE ~ as.character(Area)  # Pour gérer les éventuels cas non couverts
+  ))
 
 
 
-
-# Calculer le tableau de contingence
-tableau <- table(db_final$N_doors, db_final$N_claims_year)
-ggplot(data = db_final, aes(x = factor(N_doors), fill = factor(N_claims_year))) +
-  geom_bar(position = "fill") +
-  labs(x = "nb_doors", y = "Proportion", fill = "nb claims") +
-  theme_minimal() +
-  coord_flip()
-# Afficher le tableau
-print(tableau)
+db_final <- db_final %>%
+  mutate(Second_driver= case_when(
+    Second_driver == 0 ~ "no",
+    Second_driver == 1 ~ "yes",
+    TRUE ~ as.character(Second_driver)  # Pour gérer les éventuels cas non couverts
+  ))
 
 
-ggplot(db_final, aes(x = Type_fuel, y = Premium)) + 
-  geom_boxplot() +
-  labs(x = "Nom de Variable Catégorielle", y = "Nom de Variable Numérique") +
-  ggtitle("Relation entre  et ")
+# to get age instead of birthdate, easier to categorise 
+
+
+# creating classes per age 
+db_final$Premium_class <- case_when(db_final$Premium>0 & db_final$Premium<=150 ~"15-25",
+                          db_final$Premium>150 & db_final$Premium<=300~ "25-35",
+                          db_final$Premium>300 & db_final$Premium<=450~"35-45",
+                          db_final$Premium>450 & db_final$Premium<=600~"45-55",
+                          db_final$Premium>600 ~"600+")
+db_final$Premium<-as.factor(db_final$Premium)
+# to get licence age, added +1, because new drivers would have value of 0 
+
+
+
+db_final$age<- 2018- as.numeric(db_final$Birth_year) 
+db_final$age <- case_when(db_final$age>15 & db_final$age<=25 ~"15-25",
+                          db_final$age>25 & db_final$age<=35~ "25-35",
+                          db_final$age>35 & db_final$age<=45~"35-45",
+                          db_final$age>45 & db_final$age<=55~"45-55",
+                          db_final$age>55 & db_final$age<=65~"55-65",
+                          db_final$age>65 & db_final$age<=75~"65-75",
+                          db_final$age>75 & db_final$age<=80~"75-85",
+                          db_final$age>80 ~"80+")
+db_final$age<-as.factor(db_final$age)
+
+db_final$Licence_time<- 2018- as.numeric(db_final$Licence_year)+ 1 
+#creation of classes for agence nd brokers instead of 0 and 1 
+db_final <- db_final %>%
+  mutate(Distribution_channel= case_when(
+    Distribution_channel == 0 ~ "agency",
+    Distribution_channel == 1 ~ "brokers",
+    TRUE ~ as.character(Distribution_channel)
+  ))
+
+#creation of class for payment 
+db_final <- db_final %>%
+  mutate(Payment= case_when(
+    Payment == 0 ~ "annual",
+    Payment == 1 ~ "semester",
+    TRUE ~ as.character(Area)  # Pour gérer les ?ventuels cas non couverts
+  ))
+
+db_final$Categorie <- cut(db_final$Premium, breaks = c(seq(0, 600, by = 150), Inf), include.lowest = TRUE)
+
+# Compter le nombre de polices dans chaque catégorie
+nombre_polices <- table(db_final$Categorie)
+
+# Afficher le nombre de polices dans chaque catégorie
+print(nombre_polices)
+
+
+
+db_final$Lapse <- as.factor(db_final$Lapse)
+db_final$Seniority <- as.factor(db_final$Seniority)
+db_final$Policies_in_force<- as.factor(db_final$Policies_in_force)
+db_final$N_doors<- as.factor(db_final$N_doors)
+db_final$Power<- as.factor(db_final$Power)
+db_final$Year_matriculation<- as.numeric(db_final$Year_matriculation)
+db_final$Start_year<- as.factor(db_final$Start_year)
+db_final$Licence_time<- as.factor(db_final$Licence_time)
+db_final$N_claims_history<- as.factor(db_final$N_claims_history)
+db_final$N_claims_year<- as.factor(db_final$N_claims_year)
+db_final$Cost_claims_year<- as.factor(db_final$Cost_claims_year)
+db_final$R_Claims_history<- as.factor(db_final$R_Claims_history)
+
+db_final <- subset(db_final, select = -Birth_year)
+db_final <- subset(db_final, select = -Licence_year)
