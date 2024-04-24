7cd7e54fddfab973f253cf60c1e44b07a521d219
ZachHebdo <45517776+ZachHebdo@users.noreply.github.com>
2024-04-24 10:38:39 +0200

    graphs and some calculations

--- PCA.R
+++ PCA.R
@@ -26,6 +26,8 @@ df$N_claims_history <- as.factor(df$N_claims_history)
 
 df_quant <- df %>% select(where(is.numeric))
 df_qual <- df %>% select(where(~ !is.numeric(.)))
+## make sure to run the function for plotting the circle at the end of the code 
+
 
 #descriptive analysis 
 summary(df_quant)
@@ -54,7 +56,9 @@ attributes(pca)
 biplot(pca)
 eigenvectors=pca$loadings
 eigenvalues=pca$sdev^2.
-pca$scores
+pca$scores[,1:2]
+#have to do this to have same direction of graph 
+PCA$vectors = -1 * PCA$vectors
 
 #normal way to calculate scores 
 scores = scale(df_quant) %*% PCA$vectors 
@@ -72,22 +76,16 @@ cor3 = sqrt(PCA$values[3])*PCA$vectors[,3]
 cor4 = sqrt(PCA$values[4])*PCA$vectors[,4]
 
 #graph of interpretation 
-plot(cor1, cor2, xlim=c(-1,1), ylim=c(-1,1), xlab="PC1", ylab="PC2")
-abline(h=0, v=0)
-symbols(0, 0, circles=1, inches=FALSE, add=TRUE)
-text(cor1, cor2, labels=colnames(df_quant), cex=0.7)
+plot_pca_circle(cor1, cor2)
+plot_pca_circle(cor2, cor3)
+plot_pca_circle(cor3, cor4)
 
-# Plot for PC3 and PC4
-plot(cor3, cor4, xlim=c(-1,1), ylim=c(-1,1), xlab="PC3", ylab="PC4")
-abline(h=0, v=0)
-symbols(0, 0, circles=1, inches=FALSE, add=TRUE)
-text(cor3, cor4, labels=colnames(df_quant), cex=0.7)
 
 
 # Apply robust PCA on your dataset (If there are less than 50000 observations and less than 20 variables then the MCD is used.)
 robust_cov <- covRob(df_quant, corr = TRUE, estim = 'weighted')
 PCA_robust <- eigen(robust_cov$cov)
-PCA_robust$vectors
+PCA_robust$vectors = -1* PCA_robust$vectors
 PCA_robust$values
 
 #plotting and looks like we should keep around 3 or 4 dimensions 
@@ -106,7 +104,13 @@ cor_rob3 = sqrt(PCA_robust$values[3])*PCA_robust$vectors[,3]
 plot(cor_rob1, cor_rob2, xlim=c(-1,1), ylim=c(-1,1), xlab="PC1", ylab="PC2")
 abline(h=0, v=0)
 symbols(0, 0, circles=1, inches=FALSE, add=TRUE)
-text(cor1, cor2, labels=colnames(df_quant), cex=0.7, pos=1, offset=0.1)
+text(cor1, cor2, labels=colnames(df_quant), cex=0.7, offset=0.1)
+
+
+# Plot
+plot_pca_circle(cor_rob1, cor_rob2)
+plot_pca_circle(cor_rob2, cor_rob3)
+plot_pca_circle(cor_rob1, cor_rob3)
 
 #calculating scores 
 scores_robust = scale(df_quant) %*% PCA_robust$vectors 
@@ -116,45 +120,90 @@ scores_robust = scale(df_quant) %*% PCA_robust$vectors
 autoplot(pca, data= df, colour="Type_risk",loadings = TRUE, loadings.colour = 'blue',
          loadings.label = TRUE, loadings.label.size = 3)
 
+
 ### IDEA: Keep only three principal components, to favour interpretability of the dataset. 
+scores = as.data.frame(scores)
+colnames(scores) = paste("PC", 1:7, sep = "")
+typerisk = df$Type_risk
+scores$Typerisk = typerisk
+
+loadings = as.data.frame(PCA$vectors) 
+loadings$variable = colnames(df_quant)
+colnames(loadings) = paste("PC", 1:7, sep = "")
+colnames(loadings)[8] = "Label"
+
+#biplots for the different non-robust pca components 
+
+scale_factor = 12
+ggplot(scores, aes(x = PC2, y = PC3,color=typerisk)) +
+  geom_point(alpha =0.7) +  # Add points
+  theme_minimal() +
+  geom_segment(data = loadings, aes(x = 0, y = 0, xend = PC2 * scale_factor, yend = PC3 * scale_factor), 
+               arrow = arrow(type = "closed", length = unit(0.1, "inches")), color = "blue") +
+  labs(title = "Basic PCA Biplot", x = "PC1", y = "PC2") +
+  geom_text(data = loadings, aes(x = PC2 * scale_factor, y = PC3 * scale_factor, label = Label), 
+          hjust = 0, vjust = 0, color = "blue", size = 3, nudge_x = 0.02, nudge_y = 0.02) 
+
+ggplot(scores, aes(x = PC1, y = PC2,color=typerisk)) +
+  geom_point(alpha =0.7) +  # Add points
+  theme_minimal() +
+  geom_segment(data = loadings, aes(x = 0, y = 0, xend = PC1 * scale_factor, yend = PC2 * scale_factor), 
+               arrow = arrow(type = "closed", length = unit(0.1, "inches")), color = "blue") +
+  labs(title = "Basic PCA Biplot", x = "PC1", y = "PC2") +
+  geom_text(data = loadings, aes(x = PC1 * scale_factor, y = PC2 * scale_factor, label = Label), 
+            hjust = 0, vjust = 0, color = "blue", size = 3, nudge_x = 0.02, nudge_y = 0.02) 
 
 
 
 
+scores_robust = as.data.frame(scores_robust)
+colnames(scores_robust) = paste("PC", 1:7, sep = "")
+typerisk = df$Type_risk
+scores_robust$Typerisk = typerisk
+
+loadings_robust = as.data.frame(PCA_robust$vectors) 
+loadings_robust$variable = colnames(df_quant)
+colnames(loadings_robust) = paste("PC", 1:7, sep = "")
+colnames(loadings_robust)[8] = "Label"
+
+#biplots for the robust princicipal components 
+ggplot(scores_robust, aes(x = PC1, y = PC2,color=typerisk)) +
+  geom_point(alpha =0.7) +  # Add points
+  theme_minimal() +
+  geom_segment(data = loadings_robust, aes(x = 0, y = 0, xend = PC1 * scale_factor, yend = PC2 * scale_factor), 
+               arrow = arrow(type = "closed", length = unit(0.1, "inches")), color = "blue") +
+  labs(title = "Basic PCA Biplot", x = "PC1", y = "PC2") +
+  geom_text(data = loadings_robust, aes(x = PC1 * scale_factor, y = PC2 * scale_factor, label = Label), 
+            hjust = 0, vjust = 0, color = "blue", size = 3, nudge_x = 1.03, nudge_y = 0.03) 
 
 
 
-#visualization of the circle with a function, have to work on this 
-plot_pca_correlation <- function(cor1, cor2, dataset) {
-  # Create a data frame for ggplot
-  pca_data <- data.frame(
-    PC1 = cor1,
-    PC2 = cor2,
-    Variable = colnames(dataset)
-  )
+#function to visualize the correlation circle 
+plot_pca_circle <- function(x, y) {
+  # Create a data frame from the vectors
+  dftest <- data.frame(PCx = x, PCy = y, Labels = colnames(df_quant))
   
-  # Define the limits for the plot
-  limits <- max(abs(c(cor1, cor2))) * 1.1  # Slightly increase limits to ensure space for text
+  # Determine the names for the PC axes dynamically based on input
+  x_name <- deparse(substitute(x))
+  y_name <- deparse(substitute(y))
   
-  # Create the plot
-  p <- ggplot(pca_data, aes(x = PC1, y = PC2, label = Variable)) +
-    geom_hline(yintercept = 0, color = "black") +  # Solid line for x-axis
-    geom_vline(xintercept = 0, color = "black") +  # Solid line for y-axis
-    geom_circle(aes(x0 = 0, y0 = 0, r = 1), color = "black", linetype = "solid") +  # Solid circle
-    geom_rect(aes(xmin = -1, xmax = 1, ymin = -1, ymax = 1), 
-              color = "black", fill = NA, linetype = "solid") +  # Rectangle around the circle
-    geom_point(color = "blue") +  # Add points
-    geom_text_repel() +  # Add labels
-    xlim(-limits, limits) +
-    ylim(-limits, limits) +
-    coord_fixed(ratio = 1) +  # Ensure the aspect ratio is fixed to make the circle round
-    theme_classic() +
-    theme(axis.title = element_text(size = 12), 
-          axis.text = element_text(size = 10)) +
-    labs(title = "PCA Correlation Circle", 
-         x = "PC1", 
-         y = "PC2")
+  # Create the ggplot object
+  p <- ggplot(dftest, aes(x = PCx, y = PCy)) +
+    geom_hline(yintercept = 0) +
+    geom_vline(xintercept = 0) +
+    geom_point() +
+    geom_text_repel(aes(label = Labels)) +
+    annotate("path", x = cos(seq(0, 2 * pi, length.out = 100)), 
+             y = sin(seq(0, 2 * pi, length.out = 100)), colour = "black") +
+    coord_fixed(ratio = 1) +
+    xlim(-1, 1) +
+    ylim(-1, 1) +
+    labs(x = x_name, y = y_name) +
+    theme_minimal()
   
+  # Return the plot
   return(p)
 }
-plot_pca_correlation(cor_rob1,cor_rob2,df_quant)
+
+
+
