9682a7b14cb7c7c0e6a0755a7053c851bee8ce06
omledufromage <mreverbel@protonmail.com>
2024-04-28 19:23:37 +0200

    Finished the Clustering part of the MCA scores

--- MCA.R
+++ MCA.R
@@ -1,15 +1,13 @@
 library(dplyr)
-library(robust)
 library(tidyverse)
 library(dummy)
 library(FactoMineR)
 library(factoextra)
-library(corrplot)
-library(ggcorrplot)
 library(questionr)
 library(ade4)
 library(ggrepel)
-
+library(scatterplot3d)
+library(NbClust)
 
 df = read.csv("preprocessedData.csv")
 
@@ -70,7 +68,7 @@ G_c #centre de gravité
 rowSums(data)/sum(data)
 
 # MCA 
-mca1=MCA(df, ncp = 28, graph = FALSE)
+mca1=MCA(df, ncp = 16, graph = FALSE)
 
 mca2=MCA(df, ncp = 5, graph = FALSE, method = "Burt")
 burt = acm.burt(df, df)
@@ -97,18 +95,11 @@ fviz_mca_var(mca1, geom = c("point", "text"), axes = c(1, 3), repel = TRUE, shap
 fviz_mca_var(mca1, geom = c("point", "text"), axes = c(1, 4), repel = TRUE, shape.var = 19, alpha = 0.7, 
              col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
 
-#Coordonnées des modalités sur les axes principaux
-mca1$var$coord[,1:3]
-mca1$var$eta2[,1:3]
+mca1$var$coord[,1:3] #Coordonnées des modalités sur les axes principaux
 mca1$var$contrib[,1:3] #Contribution des modalités à la construction des axes principaux
 mca1$var$cos2[,1:3] #Qualité de représentation des modalités sur les axes principaux
-mca1$var$v.test[,1:3] 
+mca1$var$eta2[,1:3] # Correlation au carré entre les VARIABLES et les axes principaux 
 
-# # First axis:
-# round(mca2$var$contrib[,1], 3)
-# round(mca2$var$contrib[,1]/sum(mca2$var$contrib[,1]), 3)
-
-# So second dimension is mainly composed of Motorcycle (vs vans) and gasoline (vs diesel), and young adult young driver
 fviz_contrib(mca1, choice = "var", axes = 1)
 fviz_contrib(mca1, choice ="var", axes = 2)
 fviz_contrib(mca1, choice ="var", axes = 3)
@@ -116,6 +107,13 @@ fviz_contrib(mca1, choice ="var", axes = 3)
 fviz_cos2(mca1, choice = "var", axes = 1)
 fviz_cos2(mca1, choice = "var", axes = 2)
 fviz_cos2(mca1, choice = "var", axes = 3)
+
+###############################################################################
+# Correlation circle (only positive values, since #eta2 is the correlation squared)
+
+fviz_mca_var(mca1, choice = "mca.cor", 
+             repel = TRUE, # Avoid text overlapping (slow)
+             ggtheme = theme_minimal())
 
 # Distance between modalities
 #------------------------------------
@@ -139,62 +137,6 @@ dist_df
 Dist_CP_Gc <- n/npl - 1
 Dist_CP_Gc
 
-fviz_mca_var(mca1, choice = "mca.cor", 
-             repel = TRUE, # Avoid text overlapping (slow)
-             ggtheme = theme_minimal())
-
-#  Calculating eigenvalues manually (Just for checking)
-element <- function(df, i, j) {
-    t <- (df[i, j] - npl[j]/n)/sqrt(P*npl[j])
-    
-    return(t)
-}
-
-T <- matrix(nrow = dim(CDT)[1], ncol = dim(CDT)[2])
-for (i in 1:dim(CDT)[1]) {
-    for (j in 1:dim(CDT)[2]) {
-        T[i, j] <- element(CDT, i, j)        
-    }
-}
-
-V <- t(T) %*% T
-W <- T %*% t(T)
-
-eigenV <- eigen(V)
-# eigenW <- eigen(W)
-
-round(eigenV$values/sum(eigenV$values), 3)
-all(near(eigenV$vectors %*% diag(eigenV$values) %*% t(eigenV$vectors), V))
-
-# Verifying that the eigenvalues are correctly calculated for all K-P relevant dimensions. Thus, T is correctly calculated as well.
-all(near(eigenV$values[1:(K-P)], mca1$eig[1:(K-P),1]))
-
-mca1$ind$coord[,1:3]
-
-PC1 <- as.matrix(row_profiles) %*% eigenV$vectors
-PC2 <- as.matrix(column_profiles) %*% eigenV$vectors
-
-pca=princomp(W, cor=FALSE)
-pca$scores
-
-### Inspect if there's any interesting relation between the qualitative variables and the Cost_claims_year variable: 
-df2 <- preprocessed_df %>% select(where(~ !is.numeric(.)) | "Cost_claims_year")
-df2 <- df2 %>% mutate(Cost_claims_year = quant.cut(Cost_claims_year, 4))
-
-K <- length(names(dummy(df2))) # Number of modalities (28)
-P <- dim(df2)[2] # Number of variables (12)
-
-cutoff = 100/P
-mca2=MCA(df2, ncp = 5, graph = FALSE, method = "Burt")
-
-fviz_screeplot(mca2, ncp = 20, addlabels = TRUE, ylim = c(0, 30)) + 
-    geom_hline(yintercept=cutoff, linetype=2, color="red")
-
-fviz_mca_var(mca2, geom = c("point", "text"), axes = c(1, 2), repel = TRUE, shape.var = 19, alpha = 0.7, 
-             col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
-
-# Nothing particularly interesting. The contribution of all the modalities of Cost_claims_year is very low to the first 2 dimensions.
-
 ################################################################################
 # CLUSTERING SUR LES SCORES DU MCA
 df2 <- df_qual
@@ -206,9 +148,9 @@ fviz_mca_var(mca3, geom = c("point", "text"), axes = c(1, 2), repel = TRUE, shap
 
 scores <- mca3$ind$coord[,1:3]
 d=dist(scores, method="euclidean") #matrice des distances euclidiennes
-clust=hclust(d, method="ward.D2") #clustering hiérarchique ascendant basé sur le type de lien simple
+clust=hclust(d, method="ward.D2", labels = FALSE) #clustering hiérarchique ascendant basé sur le type de lien simple
 plot(clust) #dendogramme
-rect.hclust(clust, k=2, border="red") #supposons que l'on veuille k=3 clusters
+rect.hclust(clust, k=2, border="red") #supposons que l'on veuille k=2 clusters
 
 scores <- cbind(scores, Cost_claims_year = preprocessed_df$Cost_claims_year, Premium = preprocessed_df$Premium)
 
@@ -216,8 +158,8 @@ groups=cutree(clust, k=2) #appartenance de chaque ménage français à l'un des 
 # scores[groups==1,] 
 # scores[groups==2,]
 
-anova(lm(as.data.frame(scores)$Cost_claims_year~groups))
-anova(lm(as.data.frame(scores)$Premium~groups))
+# anova(lm(as.data.frame(scores)$Cost_claims_year~groups))
+# anova(lm(as.data.frame(scores)$Premium~groups))
 
 select=(groups==1|groups==2) #opérateur logique "ou"
 test=t.test(as.data.frame(scores)$Cost_claims_year[select]~groups[select])
@@ -226,29 +168,38 @@ test
 test=t.test(as.data.frame(scores)$Premium[select]~groups[select])
 test
 
+# Uses 30 different indices and choses the optimal number of clusters based on which cluster is picked most by the indices.
+# Majority rule leads to the choice of 4 clusters (Uncomment to run - SLOW): 
+#### nb <- NbClust(scores, distance = "euclidean", min.nc = 2, max.nc = 10, method = "kmeans")
+
+## For KMeans, we keep the amount of clusters for which the intragroup variance begins to stabilise (variance within each group becomes stable): 
 scores <- mca3$ind$coord[,1:3]
 within=NULL
-for(i in 1:11) within[i]=sum(kmeans(scores,centers=i)$withinss)
-plot(1:11, within, type="b")
-#Nombre de clusters retenu : 5
+for(i in 1:10) within[i]=sum(kmeans(scores,centers=i)$withinss)
+plot(1:10, within, type="b")
+abline(v = 4, col = "blue", lty = 2)
+#Nombre de clusters retenu : 4
 
 #Graphique du premier plan factoriel et de l'appartenance aux clusters
-clust2=kmeans(scores, 5) #k-means avec 5 clusters
-
-plot(scores, col=clust2$cluster, pch=19, cex=2)
-abline(v=0, h=0)
-
-# plot(scores[,2:3], col=clust2$cluster, pch=19, cex=2)
-# abline(v=0, h=0)
-# 
-# plot(scores[,c(1,3)], col=clust2$cluster, pch=19, cex=2)
-# abline(v=0, h=0)
+clust2=kmeans(scores, 4) #k-means avec 5 clusters
+fviz_cluster(clust2, scores[,c(1,2)], geom = "point")
+fviz_cluster(clust2, scores[,c(1,3)], geom = "point")
+fviz_cluster(clust2, scores[,c(2,3)], geom = "point")
 
 scatterplot3d(scores, y=NULL, z=NULL, pch = 20, color = clust2$cluster)
 
-scores <- cbind(scores, Cost_claims_year = preprocessed_df$Cost_claims_year, Premium = preprocessed_df$Premium)
-anova(lm(as.data.frame(scores)$Cost_claims_year~clust2$cluster))
-anova(lm(as.data.frame(scores)$Premium~clust2$cluster))
+scores <- as.data.frame(cbind(scores, Cost_claims_year = preprocessed_df$Cost_claims_year, Premium = preprocessed_df$Premium))
+boxplot(scores$Cost_claims_year~clust2$cluster, main='Boxplot of Cost of claims in the year for each cluster')
+boxplot(scores$Premium~clust2$cluster, main='Boxplot of Premium for each cluster')
+
+
+anova(lm(scores$Cost_claims_year~clust2$cluster))
+anova(lm(scores$Premium~clust2$cluster))
+
+# Since we saw that Premium was a very variable heavily affected by outliers, we decided to do a Kruskal Wallis test. 
+# This concludes that the clusters do differ in terms of Premium, while they do not differ for the Cost_claims_year (when accounting for outliers).
+kruskal.test(scores$Cost_claims_year~clust2$cluster, data = as.data.frame(scores))
+kruskal.test(scores$Premium~clust2$cluster, data = as.data.frame(scores))
 
 # Last plot:
 fviz_mca_biplot(mca3, label = "var", repel = TRUE, alpha.ind = 0.15, col.var = "grey52", habillage = as.factor(clust2$cluster))
@@ -283,4 +234,50 @@ fviz_mca_biplot(mca3, axes = c(1, 3), label = "var", repel = TRUE, alpha.ind = 0
 
 # cor <- sqrt(mca1$var$eta2)
 # corr_circle(cor[,1], cor[,2])
+
+################################################################################
+#  Calculating eigenvalues manually (Just for checking)
+element <- function(df, i, j) {
+    t <- (df[i, j] - npl[j]/n)/sqrt(P*npl[j])
+    
+    return(t)
+}
+
+T <- matrix(nrow = dim(CDT)[1], ncol = dim(CDT)[2])
+for (i in 1:dim(CDT)[1]) {
+    for (j in 1:dim(CDT)[2]) {
+        T[i, j] <- element(CDT, i, j)        
+    }
+}
+
+V <- t(T) %*% T
+W <- T %*% t(T)
+
+eigenV <- eigen(V)
+# eigenW <- eigen(W)
+
+round(eigenV$values/sum(eigenV$values), 3)
+all(near(eigenV$vectors %*% diag(eigenV$values) %*% t(eigenV$vectors), V))
+
+# Verifying that the eigenvalues are correctly calculated for all K-P relevant dimensions. Thus, T is correctly calculated as well.
+all(near(eigenV$values[1:(K-P)], mca1$eig[1:(K-P),1]))
+
+#################################################################################
+### Inspect if there's any interesting relation between the qualitative variables and the Cost_claims_year variable: 
+df3 <- preprocessed_df %>% select(where(~ !is.numeric(.)) | "Cost_claims_year")
+df3 <- df3 %>% mutate(Cost_claims_year = quant.cut(Cost_claims_year, 4))
+
+K2 <- length(names(dummy(df3))) # Number of modalities (28)
+P2 <- dim(df3)[2] # Number of variables (12)
+
+cutoff = 100/P2
+mca_extra=MCA(df2, ncp = 5, graph = FALSE)
+
+fviz_screeplot(mca_extra, ncp = 20, addlabels = TRUE, ylim = c(0, 30)) + 
+    geom_hline(yintercept=cutoff, linetype=2, color="red")
+
+fviz_mca_var(mca_extra, geom = c("point", "text"), axes = c(1, 2), repel = TRUE, shape.var = 19, alpha = 0.7, 
+             col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
+
+# Nothing particularly interesting. The contribution of the modalities of Cost_claims_year variable is very low in the first 2 dimensions.
 
